14c872da294aa3683b40ec4488f3c71a025c2f41
Garrett Pennell <garrettpennell@gmail.com>
2023-10-04 17:04:11 -0500

    Use prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import { useState } from 'react';
-import regionsData from './assets/regions.json';
-import RegionPicker from './RegionPicker/RegionPicker';
-import RegionInstructions from './RegionInstructions/RegionInstructions';
+import { useState } from "react";
+import regionsData from "./assets/regions.json";
+import RegionPicker from "./RegionPicker/RegionPicker";
+import RegionInstructions from "./RegionInstructions/RegionInstructions";
 
 function App() {
-
-  const [selectedRegion, setSelectedRegion] = useState(Object.keys(regionsData)[0]);
+  const [selectedRegion, setSelectedRegion] = useState(
+    Object.keys(regionsData)[0],
+  );
 
   return (
     <>
-    <main>
-      <h1>DART to the Fair from {selectedRegion}</h1>
-      <RegionPicker selectedRegion={selectedRegion} setSelectedRegion={setSelectedRegion} regions={regionsData}/>
-      <div className="instructions-and-map">
-        <RegionInstructions />
-        <div className="dummy-map"></div>
-      </div>
-    </main>
+      <main>
+        <h1>DART to the Fair from {selectedRegion}</h1>
+        <RegionPicker
+          selectedRegion={selectedRegion}
+          setSelectedRegion={setSelectedRegion}
+          regions={regionsData}
+        />
+        <div className="instructions-and-map">
+          <RegionInstructions />
+          <div className="dummy-map"></div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
 export default App;
--- src/RegionPicker/RegionPicker.tsx
+++ src/RegionPicker/RegionPicker.tsx
@@ -1,35 +1,39 @@
-import React from 'react';
+import React from "react";
 
 interface RegionPickerProps {
-    selectedRegion: string;
-    setSelectedRegion: (region: string) => void;
-    regions: Record<string, { steps: string[] }>;
+  selectedRegion: string;
+  setSelectedRegion: (region: string) => void;
+  regions: Record<string, { steps: string[] }>;
 }
 
 /**
  * Show available regions and allow the user to select them.
- * 
+ *
  * @param {Object} props - React component props
  * @param {string} props.selectedRegion - The currently selected region
  * @param {Function} props.setSelectedRegion - hook to set the selected region
  * @param {Object} props.regions - object containing regions and their instructions
- * 
+ *
  * @returns {JSX.Element}
  */
-const RegionPicker: React.FC<RegionPickerProps> = ({selectedRegion, setSelectedRegion, regions}) => {
-    return(
-        <div className="region-picker">
-            {Object.keys(regions).map(region => (
-                <button
-                    key={region}
-                    onClick={() => setSelectedRegion(region)}
-                    className={region === selectedRegion ? 'selected-region' : ''}
-                >
-                    {region}
-                </button>
-            ))}
-        </div>
-    )
-}
+const RegionPicker: React.FC<RegionPickerProps> = ({
+  selectedRegion,
+  setSelectedRegion,
+  regions,
+}) => {
+  return (
+    <div className="region-picker">
+      {Object.keys(regions).map((region) => (
+        <button
+          key={region}
+          onClick={() => setSelectedRegion(region)}
+          className={region === selectedRegion ? "selected-region" : ""}
+        >
+          {region}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 export default RegionPicker;
