e65e4215dd9f1a3cba5d5bc7a32c96d44bb2eab0
Garrett Pennell <garrettpennell@gmail.com>
2023-10-11 14:32:41 -0500

    Add RegionInstructions component

--- src/RegionInstructions/RegionInstructions.tsx
+++ src/RegionInstructions/RegionInstructions.tsx
@@ -1,5 +1,29 @@
-const RegionInstructions = () => {
-  return <div className="instructions"></div>;
+import React from "react";
+interface RegionInstructionsProps {
+  selectedRegion: string;
+  regions: Record<string, { steps: string[] }>;
+}
+
+/**
+ * Show instructions for the selected region
+ * @param {Object} props - React component props
+ * @param {string} props.selectedRegion - The currently selected region
+ * @param {object} props.regions - object containing regions and their instructions
+ */
+const RegionInstructions: React.FC<RegionInstructionsProps> = ({
+  selectedRegion,
+  regions,
+}) => {
+  const region = regions[selectedRegion];
+  return (
+    <section>
+      <h2>Instructions</h2>
+      <ol>
+        {region &&
+          region.steps.map((step, index) => <li key={index}>{step}</li>)}
+      </ol>
+    </section>
+  );
 };
 
 export default RegionInstructions;
